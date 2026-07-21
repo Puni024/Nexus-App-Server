96bf8af2da3b1c27da6b82572b4e35dc4521d699
puneeth <puneethck24@gmail.com>
2026-07-21 17:33:28 +0530

    minor change in db connection

--- config/db.ts
+++ config/db.ts
@@ -1,24 +1,24 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME!,
-    process.env.DB_USER!,
-    process.env.DB_PASSWORD!,
-    {
-        host: process.env.DB_HOST!,
-        port: parseInt(process.env.DB_PORT!),
-        dialect: "postgres",
-        dialectOptions: {
-            dialectOptions: {
-                ssl:
-                    process.env.NODE_ENV === "production"
-                        ? {
-                            require: true,
-                            rejectUnauthorized: false,
-                        }
-                        : false,
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST!,
+    port: parseInt(process.env.DB_PORT!),
+    dialect: "postgres",
+
+    dialectOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
             },
-        },
-        logging: false, // optional, quiets Sequelize's SQL logging
-    }
+          }
+        : {},
+
+    logging: false,
+  }
 );
