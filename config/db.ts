9f6116a3c81ebd54d46f8f4f09dc338d7d80ee97
puneeth <puneethck24@gmail.com>
2026-07-21 16:36:04 +0530

    DB connection updating for Production

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize=new Sequelize(
+export const sequelize = new Sequelize(
     process.env.DB_NAME!,
     process.env.DB_USER!,
     process.env.DB_PASSWORD!,
@@ -9,9 +9,14 @@ export const sequelize=new Sequelize(
         port: parseInt(process.env.DB_PORT!),
         dialect: "postgres",
         dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
+            dialectOptions: {
+                ssl:
+                    process.env.NODE_ENV === "production"
+                        ? {
+                            require: true,
+                            rejectUnauthorized: false,
+                        }
+                        : false,
             },
         },
         logging: false, // optional, quiets Sequelize's SQL logging
