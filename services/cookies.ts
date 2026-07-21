4be865e0858563b0b4ff4815d680e4ebfc74410e
puneeth <puneethck24@gmail.com>
2026-07-21 12:46:53 +0530

    "cookies problem"

--- services/cookies.ts
+++ services/cookies.ts
@@ -1,7 +1,8 @@
 export const COOKIE_OPTIONS = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax" as const,
+    sameSite: "none" as const,
+    path: "/",
     maxAge: 1000*60*60*5, // 5 hours
     // maxAge: 1000 * 60 * 60, // 1 hour
 };
