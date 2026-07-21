977484a67c5004d761b876d8d5a6859aea70bb6c
puneeth <puneethck24@gmail.com>
2026-07-21 13:13:41 +0530

    cookies changes

--- services/cookies.ts
+++ services/cookies.ts
@@ -1,8 +1,8 @@
 export const COOKIE_OPTIONS = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none" as const,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     path: "/",
-    maxAge: 1000*60*60*5, // 5 hours
+    maxAge: 1000 * 60 * 60 * 5, // 5 hours
     // maxAge: 1000 * 60 * 60, // 1 hour
 };
